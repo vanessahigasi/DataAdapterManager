6ecafdc6f3e9365d1f1b8e811b167a179a9e8a84
vanessahigasi <vanessahigasi@gmail.com>
2019-05-21 10:41:38 +0200

    Add a standard ArrayAdapter for a listView

--- app/src/main/java/com/example/dataadaptermanager/MainActivity.java
+++ app/src/main/java/com/example/dataadaptermanager/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.dataadaptermanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] listItems = {"Android", "iPhone", "WindowsMobile","Blackberry", "WebOS", "Ubuntu",
+                "Windows7", "Max OS X", "Linux", "OS/2", "Ubuntu","Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux","OS/2", "Android", "iPhone",
+                "WindowsMobile"};
+
+        final ListView listView = findViewById(R.id.activity__main__lv__data);
+        listView.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,"position: " + position,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
